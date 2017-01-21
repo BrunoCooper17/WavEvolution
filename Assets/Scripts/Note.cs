678f9619a5cb3157caaf574874fed1a1eb2b2f44
Hysterias <sweetsacrifices@gmail.com>
2017-01-21 13:13:05 -0600

    Added note component

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Note : MonoBehaviour {
+
+    private Animator noteAnimation;
+    private AudioSource sound;
+    public AudioClip clipGood;
+    public AudioClip clipWrong;
+
+    // Use this for initialization
+    void Start () {
+        // Animators
+        noteAnimation = GetComponent<Animator>();
+
+        // Songs
+        sound = GetComponent<AudioSource>();
+    }
+
+    public void PlaySong(bool isGood)
+    {
+        if (isGood)
+        {
+            noteAnimation.Play("good");
+            sound.clip = clipGood;
+        } else
+        {
+            noteAnimation.Play("Bad");
+            sound.clip = clipWrong;
+        }
+
+        sound.Play();
+    }
+}
